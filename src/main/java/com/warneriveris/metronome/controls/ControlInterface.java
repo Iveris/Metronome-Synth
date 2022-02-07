4cce6b5feaf806b58a295fdc9e7e4084055860da
SuperWarnerMan <WarnerIveris@gmail.com>
2022-02-07 13:03:53 -0800

    ControlInterface defines behavior of the GUI and metronome controllers

---
+++ src/main/java/com/warneriveris/metronome/controls/ControlInterface.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.warneriveris.metronome.controls;
+
+/**
+ *
+ * @author Warner Iveris
+ */
+public interface ControlInterface {
+
+    void increment();
+    void decrement();
+    void stopPlay();
+    
+}
